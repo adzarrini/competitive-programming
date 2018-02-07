be8362993232e5edd3a45a4389864f7909dd6dc9
adzarrini <adzarrini@mymail.mines.edu>
2018-02-07 15:19:20 -0700

    A passing!

--- src/sp18Set4/A.java
+++ src/sp18Set4/A.java
@@ -22,29 +22,9 @@ public class A {
 		double dang = Math.PI / M;
 		double dr = (R / N);
 		
-		double[][] grid = new double[M][N];
-		for(int i = 0; i < M; i++) {
-			for(int j = 0; j < N; j++) {
-				grid[i][j] = Double.MAX_VALUE;
-			}
-		}
-		ArrayList<Double> adj = new ArrayList<Double>();
-		for(int i = 0; i < M; i++) {
-			for(int j = 0; j < N; j++) {
-				if((ax+i)%M+1 < M) adj.add(grid[(ax+i)%M+1][(ay+j)%N]+dang*((ay+j)%N));
-				if((ax+i)%M-1 >= 0) adj.add(grid[(ax+i)%M-1][(ay+j)%N]+dang*((ay+j)%N));
-				if((ay+j)%N+1 < N) adj.add(grid[(ax+i)%M][(ay+j)%N+1]+dr);
-				if((ay+j)%N-1 >= 0) adj.add(grid[(ax+i)%M][(ay+j)%N-1]+dr);
-				double min = Double.MAX_VALUE;
-				for (int k = 0; k < adj.size(); k++) {
-					if(min > adj.get(k)) min = adj.get(k);
-				}
-				grid[i][j] = Math.min(min, grid[(ax+i)%M][(ay+j)%N]);
-			}
-			
-		}
+		double dist1 = Math.abs(ax-bx)*dang*Math.min(ay, by)*dr + Math.abs(ay-by)*dr;
+		double dist2 = ay*dr+by*dr;
 
-		System.out.println(grid[bx][by]);
+		System.out.println(Math.min(dist1,dist2));
 	}
-	
 }
