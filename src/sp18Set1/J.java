3c2b99673e149faface00677072bd4ec904be496
adzarrini <adzarrini@mymail.mines.edu>
2018-02-07 15:40:51 -0700

    Got rid of some lingering warnings from unused libraries

--- src/sp18Set1/J.java
+++ src/sp18Set1/J.java
@@ -2,8 +2,6 @@ package sp18Set1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class J {
--- src/sp18Set4/A.java
+++ src/sp18Set4/A.java
@@ -2,7 +2,6 @@ package sp18Set4;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 // Amsterdam Distance
 public class A {
